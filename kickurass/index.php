9361620e97c068d7f2b9eeb44e45f64d99ccad42
Conner Tolley <connertolley@gmail.com>
2012-04-15 15:48:34 -0500

    removed random image php stuff

--- kickurass/index.php
+++ kickurass/index.php
@@ -9,10 +9,8 @@ get_header(); the_post();
 
 			<div id="container">
 				<div id="phrases">
-					<h2><?php echo $images[1][$randImage]; ?><h3>
 				</div>
 				<div id="imageHolder">
-					<img src="<?php echo $images[0][$randImage]; ?>" alt="Kick Ass Image" />
 				</div>
 			</div>
 
