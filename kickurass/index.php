6cacadc6e030507074a1c98398ce3c5f4e1fc4bc
Conner Tolley <connertolley@gmail.com>
2012-04-15 15:48:02 -0500

    Removed reload page on arrow clicks

--- kickurass/index.php
+++ kickurass/index.php
@@ -7,9 +7,6 @@ get_header(); the_post();
 ?>
 
 
-			<a href="javascript:location.reload(true)">
-				<img src="<?php echo(get_bloginfo('template_url')); ?>/images/arrow_left.png" alt="left arrow" id="left_arrow" />
-			</a>
 			<div id="container">
 				<div id="phrases">
 					<h2><?php echo $images[1][$randImage]; ?><h3>
@@ -18,10 +15,6 @@ get_header(); the_post();
 					<img src="<?php echo $images[0][$randImage]; ?>" alt="Kick Ass Image" />
 				</div>
 			</div>
-			<a href="javascript:location.reload(true)">
-				<img src="<?php echo(get_bloginfo('template_url')); ?>/images/arrow_right.png" alt="right arrow" id="right_arrow" />
-			</a>
-		</div>
 
 	</body>
 </html>
